f115223f64b717ebe465e00819874839f86559e0
Genraim <egarmin95@gmail.com>
2015-10-05 23:25:20 +0500

    Add solution for task

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,25 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
 
-// Немного замечательного кода и магии
+var arabNum = [1, 4, 5, 9, 10, 40, 50];
+var romanNum = ["I", "IV", "V", "IX", "X", "XL", "L"];
+var res = "";
+if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
+    process.stdout.write("Время указано не верно");
+    return;
+}
+function convert(num) {
+    var res = "";
+    var i = arabNum.length - 1;
+    while(num > 0) {
+        if (num >= arabNum[i]) {
+            res += romanNum[i];
+            num -= arabNum[i];
+        }
+        else i--;
+    }
+    return res;
+}
+var result = convert(hours) + ":" + convert(minutes);
+process.stdout.write(result);
+
